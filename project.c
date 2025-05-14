ba518888ea01466725e9d0580bebbcd75f38ed89
poll0ui5 <pl.ledoux@yahoo.fr>
2025-05-14 14:57:02 +0200

    suite

--- project.c
+++ project.c
@@ -22,35 +22,60 @@ int calculatePriority(int Symptomes[]){
     return priority;
 }
 
-void ajouterPatientParPriorite(Patient **head, Patient *newPatient) {
-    if (head == NULL || *head == NULL) { // Si la liste est vide
-        *head = newPatient;
-        newPatient->next = NULL;
-        // end = *head; // Assurez-vous que 'end' est bien déclaré globalement si vous l'utilisez
-    } else {
-        if (newPatient->Priority == 1) {
-            newPatient->next = *head;
-            *head = newPatient;
-        } else if (newPatient->Priority == 2) {
-            if ((*head)->Priority >= 2) {
-                newPatient->next = *head;
-                *head = newPatient;
-            } else {
-                Patient *current = *head;
-                while (current->next != NULL && current->next->Priority == 1) {
-                    current = current->next;
-                }
-                newPatient->next = current->next;
-                current->next = newPatient;
-                // if (newPatient->next == NULL) end = newPatient;
-            }
-        }
+
+
+Patient*  ReadPatientCSV(){
+    FILE *file = fopen("patientTbT.csv", "r");
+    if (file == NULL) {
+        printf("Erreur : impossible d'ouvrir le fichier.\n");
+        return NULL;
     }
-}
 
+    char line[256]; //tableau pour stocker une ligne du fichier csv
+    int lineNumber=0; //ligne du tableau lue
+    Patient *head=NULL; //pointe vers la tête de la liste chaînée
+    Patient *end=NULL; //fin de la liste
+
+
+    while (fgets(line, sizeof(line), file)){  //fgets lis les lignes du csv une par une
+        if (lineNumber == 0){
+            lineNumber++; //pour éviter de lire la première ligne avec les catégories
+            continue; //saute le reste de la boucle
+        }
+
+        Patient *newPatient = (Patient *)malloc(sizeof(Patient));
+        if (!newPatient) {
+            printf("Erreur d'allocation mémoire.\n");
+            fclose(file);
+            return NULL;
+        }
+
+        int column=0;
+        char *token=(strtok(line, ",")); //enlève les virgules de la ligne et met des \0 à la place. strtok renvoie un pointeur vers un morceau de la chaîne d'origine (par exemple vers le SSN). 
+
+        /*On va se servir de strcpy, qui sert à copier chaque chaîne de caractère jusqu'à \0 ; strcpy(destination, pointeur argument)*/
+
+        while (token != NULL) {
+            if (column==0) strcpy(newPatient->ssn, token);
+
+            else if(column==1) strcpy(newPatient->age, token);
+
+            else if (column == 3) strcpy(newPatient->DateIn, token); 
+
+            else if (column == 4) strcpy(newPatient->TimeIn, token); 
+
+            else if(column>4){
+                // Symptômes (colonnes 6 à 10)
+                newPatient->Symptomes[column - 5] = atoi(token);  // Convertir les chaînes en int (0 ou 1)
+            }
+            token = strtok(NULL, ",");
+            column++;
+        }
+
+        newPatient->Priority = calculatePriority(newPatient->Symptomes) ;
+        newPatient->next = NULL;
         
-
-if (head==NULL){ //Si la liste est vide, donc pour la première itération
+        if (head==NULL){ //Si la liste est vide, donc pour la première itération
             head=newPatient;
             head->next=NULL;
             end=head;
@@ -78,67 +103,16 @@ if (head==NULL){ //Si la liste est vide, donc pour la première itération
                 end = newPatient;
             }
         }
-
-
-int  ReadPatientCSV(){
-    FILE *file = fopen("patientTbT.csv", "r");
-    if (file == NULL) {
-        printf("Erreur : impossible d'ouvrir le fichier.\n");
-        return 1;
     }
 
-    char line[256]; //tableau pour stocker une ligne du fichier csv
-    int lineNumber=0; //ligne du tableau lue
-    Patient *head=NULL; //pointe vers la tête de la liste chaînée
-
-
-    while (fgets(line, sizeof(line), file)){  //fgets lis les lignes du csv une par une
-        if (lineNumber == 0){
-            lineNumber++; //pour éviter de lire la première ligne avec les catégories
-            continue; //saute le reste de la boucle
-        }
-
-        Patient *newPatient = (Patient *)malloc(sizeof(Patient));
-        if (!newPatient) {
-            printf("Erreur d'allocation mémoire.\n");
-            fclose(file);
-            return 1;
-        }
-
-        int column=0;
-        char *token=(strtok(line, ",")); //enlève les virgules de la ligne et met des \0 à la place. strtok renvoie un pointeur vers un morceau de la chaîne d'origine (par exemple vers le SSN). 
-
-        /*On va se servir de strcpy, qui sert à copier chaque chaîne de caractère jusqu'à \0 ; strcpy(destination, pointeur argument)*/
-
-        while (token != NULL) {
-            if (column==0) strcpy(newPatient->ssn, token);
-
-            else if(column==1) strcpy(newPatient->age, token);
-
-            else if (column == 3) strcpy(newPatient->DateIn, token); 
-
-            else if (column == 4) strcpy(newPatient->TimeIn, token); 
-
-            else if(column>4){
-                // Symptômes (colonnes 6 à 10)
-                newPatient->Symptomes[column - 5] = atoi(token);  // Convertir les chaînes en int (0 ou 1)
-            }
-            token = strtok(NULL, ",");
-            column++;
-        }
-
-        newPatient->Priority = calculatePriority(newPatient->Symptomes) ;
-        newPatient->next = NULL;
-
-        //Ici, il faudra ajouter la fonction qui ajoute le patient à la liste chainée en fonction de sa priorité
-
-        lineNumber++;
-        }
-    fclose(file);
+    else {  // Priority == 3
+        end->next = newPatient;
+        end = newPatient;
+    }
 }
 
 int main(){
-    ReadPatientCSV();
-
+    return 0;
 }
+
 
